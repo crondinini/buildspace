990738b0aa77d85e67ec6733d7dc7925e64f9062
Camila Rondinini <crondinini@gmail.com>
2021-11-26 11:59:00 +0100

    feat: change URL for base64 encoded JSON

--- nft/contracts/MyNFT.sol
+++ nft/contracts/MyNFT.sol
@@ -26,7 +26,10 @@ contract MyNFT is ERC721URIStorage {
 
         // The token URI is what's going to hold
         // the data on the NFT.
-        _setTokenURI(newItemId, "https://jsonkeeper.com/b/KMDT");
+        _setTokenURI(
+            newItemId,
+            "data:application/json;base64,ewogICAgIm5hbWUiOiAiVGhyZWUgV29yZCBORlQiLAogICAgImRlc2NyaXB0aW9uIjogIldoYXQgZG8geW91IG5lZWQ/IiwKICAgICJpbWFnZSI6ICJkYXRhOmltYWdlL3N2Zyt4bWw7YmFzZTY0LFBITjJaeUI0Yld4dWN6MGlhSFIwY0RvdkwzZDNkeTUzTXk1dmNtY3ZNakF3TUM5emRtY2lJSEJ5WlhObGNuWmxRWE53WldOMFVtRjBhVzg5SW5oTmFXNVpUV2x1SUcxbFpYUWlJSFpwWlhkQ2IzZzlJakFnTUNBek5UQWdNelV3SWo0S0lDQWdJRHh6ZEhsc1pUNHVZbUZ6WlNCN0lHWnBiR3c2SUhkb2FYUmxPeUJtYjI1MExXWmhiV2xzZVRvZ1NHVnNkbVYwYVdNc0lITmhibk10YzJWeWFXWTdJR1p2Ym5RdGMybDZaVG9nTVRSd2VEc2dabTl1ZEMxM1pXbG5hSFE2SUdKdmJHUTdJSDA4TDNOMGVXeGxQZ29nSUNBZ1BISmxZM1FnZDJsa2RHZzlJakV3TUNVaUlHaGxhV2RvZEQwaU1UQXdKU0lnWm1sc2JEMGlJME00UVRKRE9DSWdMejRLSUNBZ0lEeDBaWGgwSUhnOUlqVXdKU0lnZVQwaU5UQWxJaUJqYkdGemN6MGlZbUZ6WlNJZ1pHOXRhVzVoYm5RdFltRnpaV3hwYm1VOUltMXBaR1JzWlNJZ2RHVjRkQzFoYm1Ob2IzSTlJbTFwWkdSc1pTSStRbkpsWVhSb1pTQkdiMk4xY3lCTVpXRnliand2ZEdWNGRENEtQQzl6ZG1jKyIKfQ=="
+        );
 
         _tokenIds.increment();
 
